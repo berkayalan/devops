98f87e8c51e93b56dddbda65c4bb1bbb418c4a55
Berkay Alan <berkayalan.mail@gmail.com>
2022-05-01 14:10:15 +0300

    sql added

---
+++ sql/creating_database.sql
@@ -0,0 +1,8 @@
+create table public.name_basics(
+nconst varchar primary key ,
+primary_name varchar,
+birth_year varchar,
+death_year varchar,
+primary_profession varchar,
+known_for_titles varchar
+);
