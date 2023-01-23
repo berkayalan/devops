dabaaad3543cef2faf3878512e59d8e54b62f5d0
berkayalan <berkayalan.mail@gmail.com>
2023-01-23 15:32:06 +0300

    deleted files corrected

--- git-patch.py
+++
@@ -1,8 +0,0 @@
-
-print("This will be moved")
-
-print("--This one also will be moved")
-
-print("--Let's see!!! But not this one!!")
-
-"""for git patch, use this syntax : git add -p git-patch.py(file name)"""
