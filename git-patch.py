61b3c84262985036b5bfd92b633775a5b3f5eefc
berkayalan <berkayalan.mail@gmail.com>
2023-01-23 14:05:34 +0300

    patch changed

--- git-patch.py
+++ git-patch.py
@@ -1,6 +1,6 @@
 
 print("This will be moved")
 
-print("This one also will be moved")
+print("--This one also will be moved")
 
-print("Let's see!!! But not this one!!")
+print("--Let's see!!! But not this one!!")
